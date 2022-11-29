f5a210af935bb41529f97ab7ecdfedd090c080cb
Bruno Pascoal <brunompp98@gmail.com>
2022-11-29 09:36:51 +0000

    added  feedback model

---
+++ app/Models/Feedback.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Feedback extends Model
+{
+    public $timestamps  = false;
+
+    protected $table = 'feedback';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'post_id', 'is_like'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post() {
+      return $this->belongsTo(Content::class);
+    }
+}
