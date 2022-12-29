290c5d4ad0a5be84d2dd79467b866c5546387477
up201806860 <up201806860@fe.up.pt>
2022-12-29 23:47:01 +0000

    admin pannel

---
+++ resources/views/pages/admin/manageTags.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+<script type="text/javascript" src="{{ asset('js/topics.js') }}"></script>
+
+@section('title', "- Manage topics")
+
+@section('content')
+
+<div class="text-center container">
+    <h1 class="text-center mt-5">Manage topics</h1>
+    <div class="mb-5 row">
+
+        <div class="px-2 mt-5 col-12 col-lg-4">
+            <div class="border bg-dark statusContainer" id="acceptedtopicsContainer">
+                <h3 class="mt-5">Accepted topics</h3>
+                @foreach ($topics_accepted as $topic)
+                    <div class="mt-5 pb-3 pt-5 bg-dark mb-5 managetopicContainer">
+                        <div id="stateButton" class="d-flex align-items-center">
+                            <h5 class="mx-3 my-0 py-0 w-75">{{ $topic['subject'] }}</h5>
+                            <button type="button" onclick="removetopic(this, {{ $topic['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
+                                <i class="fas fa-trash fa-2x mb-2 text-danger"></i>
+                            </button>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        <div class="px-2 mt-5 col-12 col-lg-4">
+            <div class="border bg-dark statusContainer" id="pendingtopicsContainer">
+                <h3 class="mt-5">Pending topics</h3>
+                @foreach ($topics_pending as $topic)
+                    <div class="mt-5 pb-3 pt-5 bg-dark mb-5 managetopicContainer">
+                        <div id="stateButton" class="d-flex align-items-center">
+                            <h5 class="mx-3 my-0 py-0 w-75">{{ $topic['subject'] }}</h5>
+                            <button type="button" onclick="accepttopic(this, {{ $topic['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
+                                <i class="fas fa-check fa-2x mb-2 text-success"></i>
+                            </button>
+                            <button type="button" onclick="rejecttopic(this, {{ $topic['id'] }})" class="my-0 mx-1 py-0 btn btn-lg btn-tranparent">
+                                <i class="fas fa-times fa-2x mb-2 text-danger"></i>
+                            </button>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        <div class="px-2 mt-5 col-12 col-lg-4">
+            <div class="border bg-dark statusContainer" id="rejecttopicsContainer">
+                <h3 class="mt-5">Rejected topics</h3>
+                @foreach ($topics_rejected as $topic)
+                    <div class="mt-5 pb-3 pt-5 bg-dark mb-5 managetopicContainer">
+                        <div id="stateButton" class="d-flex align-items-center">
+                            <h5 class="mx-3 my-0 py-0 w-75">{{ $topic['subject'] }}</h5>
+                            <button type="button" onclick="accepttopic(this, {{ $topic['id'] }})" class="my-0 py-0 btn btn-lg btn-transparent">
+                                <i class="fas fa-check fa-2x mb-2 text-success"></i>
+                            </button>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
